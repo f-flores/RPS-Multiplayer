2e0b1eb012786236e0328a8b0cec073d952b887f
Fabian Flores <ffflores1@outlook.com>
2018-03-31 15:44:00 -0400

    more begin game logic

--- assets/js/multi-rps.js
+++ assets/js/multi-rps.js
@@ -16,6 +16,8 @@ $(document).ready(() => {
   // initializes rpsGame
   function initGame() {
     console.log("in initGame()");
+    rpsGame.isPlayer1loggedin();
+    rpsGame.isPlayer1loggedin();
     // is user1 present?
     console.log("isPlayer1loggedin() in initGame(): " + rpsGame.isPlayer1loggedin());
     // is user2 present?
