53eecb3cd03847582fbbf515c9a16a60a9fe2ba3
Fabian Flores <ffflores1@outlook.com>
2018-04-04 07:48:41 -0400

    more begin game logic

--- assets/js/multi-rps.js
+++ assets/js/multi-rps.js
@@ -100,14 +100,14 @@ var rpsGame = {
       console.log("call to new PlayerConsole(), player 1");
       player1 = new PlayerConsole(pName, "1");
       console.log("player1: " + JSON.stringify(player1));
-      player1.displayName();
+      // player1.displayName();
       // player1.showOpponentName();
       player1.welcomeMsg("Hi, " + pName + "! You are player " + numPlayer + ".");
     } else if (numPlayer === "2") {
       console.log("call to new PlayerConsole(), player 2");
       player2 = new PlayerConsole(pName, "2");
       console.log("player2: " + JSON.stringify(player2));
-      player2.displayName();
+      // player2.displayName();
       // player2.showOpponentName();
       player2.welcomeMsg("Hi, " + pName + "! You are player " + numPlayer + ".");
     }
